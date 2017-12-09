36e471a31b760b7fcbef34acb80ccc10d475e5f1
Basil Nikolopoulos <nikolopoulosbasil@gmail.com>
2017-12-09 16:19:15 +0200

    new experiment and change in cache to 10 sec

--- Main_Code/MicazAggregator/src/SensorsCommunicationUnit/MicazMote.java
+++ Main_Code/MicazAggregator/src/SensorsCommunicationUnit/MicazMote.java
@@ -258,7 +258,7 @@ public class MicazMote {
         for (Service s : servicesList) {
             //System.out.println(s.getURI() + " vs " + ServiceURI);
             //30000 is cache
-            long cacheMs = 1000;
+            long cacheMs = 10000;
             if (s.getURI().contains(ServiceURI) && cached && s.getLatestReading() - System.currentTimeMillis() < cacheMs && s.getDecimalValue() != null) {
                 reply = "\"ID\":\"" + getId() + "\", \"" + s.getName() + "\":\"" + s.getDecimalValue() + "\" ";
             } else if (s.getURI().contains(ServiceURI)) {
--- loadGen01/src/loadgen01/LoadGen01.java
+++ loadGen01/src/loadgen01/LoadGen01.java
@@ -5,9 +5,11 @@
  */
 package loadgen01;
 
-import Services.DOSAttack;
-import Services.Temperature;
-import java.util.Random;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,127 +22,313 @@ public class LoadGen01 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, Exception {
         //new GUIServices.Temperature();
         //new GUIServices.FireWatchdog();
 
         //new GUIServices.Whereabouts();
-        //start phase 1
-        int phase = 0;
-        phase++;
+        //DOSAttack ddos = new DOSAttack(1, 200);
+        //Thread.sleep(20000);
+        //ddos.stop();
+        //System.out.println("Ending phase " + phase);
+        String fileName = "clientOutput.csv";
+        initFile(fileName);
 
-        System.out.println("Starting phase " + phase);
-        System.out.println("Step 1");
-        DOSAttack ddos = new DOSAttack(1, 20);
-        Thread.sleep(20000);
-        ddos.stop();
-        System.out.println("Step 2");
-        ddos = new DOSAttack(1, 500);
-        Thread.sleep(10000);
-        ddos.stop();
-        System.out.println("Step 3");
-        ddos = new DOSAttack(1, 1000);
-        Thread.sleep(10000);
-        ddos.stop();
-        System.out.println("Step 4");
-        ddos = new DOSAttack(1, 2000);
-        Thread.sleep(20000);
-        ddos.stop();
-        System.out.println("Step 5");
-        ddos = new DOSAttack(1, 1000);
-        Thread.sleep(10000);
-        ddos.stop();
-        System.out.println("Step 6");
-        ddos = new DOSAttack(1, 500);
-        Thread.sleep(10000);
-        ddos.stop();
-        System.out.println("Ending phase " + phase);
-        //end phase 1
+        long start, receive, total;
+        int crit = 0;
+        int id =0;
+        long diff = 0;
         
-        //start phase 2
-        phase++;
-        System.out.println("Starting phase " + phase);
-        System.out.println("Step 1");        
-        ddos = new DOSAttack(1, 200);
-        Thread.sleep(20000);
-        ddos.stop();
-        System.out.println("Step 2");
-        ddos = new DOSAttack(1, 500);
-        Thread.sleep(1000);
-        ddos.stop();
-        System.out.println("Step 3");
-        ddos = new DOSAttack(1, 1000);
-        Thread.sleep(1000);
-        ddos.stop();
-        System.out.println("Step 4");
-        ddos = new DOSAttack(1, 2000);
-        Thread.sleep(10000);
-        ddos.stop();
-        System.out.println("Step 5");
-        ddos = new DOSAttack(1, 3000);
-        Thread.sleep(20000);
-        ddos.stop();
-        System.out.println("Step 6");
-        ddos = new DOSAttack(1, 2000);
-        Thread.sleep(10000);
-        ddos.stop();
-        System.out.println("Step 7");
-        ddos = new DOSAttack(1, 1000);
-        Thread.sleep(10000);
-        ddos.stop();
-        System.out.println("Step 8");
-        ddos = new DOSAttack(1, 500);
-        Thread.sleep(10000);
-        ddos.stop();
-        System.out.println("Ending phase " + phase);
-        //end phase 2
+        //start
+        id++;
+        diff = 0;
+        crit = 1;
+        start = System.currentTimeMillis();
+        String url = "http://192.168.2.10:8181/sensor/2/temp?crit="+crit;
+        HTTPRequest.sendGet(url);
+        receive = System.currentTimeMillis();
+        total = receive - start;
+        logToFile(fileName,id,crit,diff,start,receive,total);
+        //prime unit with data from sensor 2
+        //this data will remain for 10 sec in cache.
+        Thread.sleep(500);
+        //End Request
+        
+        //start
+        id++;
+        diff = System.currentTimeMillis() - receive;
+        crit = 1;
+        start = System.currentTimeMillis();
+        url = "http://192.168.2.10:8181/sensor/2/temp?crit="+crit;
+        HTTPRequest.sendGet(url);
+        receive = System.currentTimeMillis();
+        total = receive - start;
+        logToFile(fileName,id,crit,diff,start,receive,total);
+        //prime unit with data from sensor 2
+        //this data will remain for 10 sec in cache.
+        Thread.sleep(500);
+        //End Request
+        
+        //start
+        id++;
+        diff = System.currentTimeMillis() - receive;
+        crit = 1;
+        start = System.currentTimeMillis();
+        url = "http://192.168.2.10:8181/sensor/2/temp?crit="+crit;
+        HTTPRequest.sendGet(url);
+        receive = System.currentTimeMillis();
+        total = receive - start;
+        logToFile(fileName,id,crit,diff,start,receive,total);
+        //prime unit with data from sensor 2
+        //this data will remain for 10 sec in cache.
+        Thread.sleep(500);
+        //End Request
 
-        //start phase 3
-        phase++;
-        System.out.println("Starting phase " + phase);
-        System.out.println("Step 1");
-        ddos = new DOSAttack(1, 200);
-        Thread.sleep(20000);
-        ddos.stop();
-        System.out.println("Step 2");
-        ddos = new DOSAttack(1, 500);
-        Thread.sleep(1000);
-        ddos.stop();
-        System.out.println("Step 3");
-        ddos = new DOSAttack(1, 1000);
-        Thread.sleep(1000);
-        ddos.stop();
-        System.out.println("Step 4");
-        ddos = new DOSAttack(1, 2000);
-        Thread.sleep(5000);
-        ddos.stop();
-        System.out.println("Step 5");
-        ddos = new DOSAttack(1, 3000);
-        Thread.sleep(10000);
-        ddos.stop();
-        System.out.println("Step 6");
-        ddos = new DOSAttack(1, 4500);
-        Thread.sleep(20000);
-        ddos.stop();
-        System.out.println("Step 7");
-        ddos = new DOSAttack(1, 2000);
-        Thread.sleep(5000);
-        ddos.stop();
-        System.out.println("Step 8");
-        ddos = new DOSAttack(1, 1000);
-        Thread.sleep(10000);
-        ddos.stop();
-        System.out.println("Step 9");
-        ddos = new DOSAttack(1, 500);
-        Thread.sleep(10000);
-        ddos.stop();
-        System.out.println("Step 10");
-        ddos = new DOSAttack(1, 200);
-        Thread.sleep(20000);
-        ddos.stop();
-        System.out.println("Ending phase " + phase);
-        //end phase 3
-
+        //start
+        id++;
+        diff = System.currentTimeMillis() - receive;
+        crit = 1;
+        start = System.currentTimeMillis();
+        url = "http://192.168.2.10:8181/sensor/2/temp?crit="+crit;
+        HTTPRequest.sendGet(url);
+        receive = System.currentTimeMillis();
+        total = receive - start;
+        logToFile(fileName,id,crit,diff,start,receive,total);
+        //prime unit with data from sensor 2
+        //this data will remain for 10 sec in cache.
+        Thread.sleep(500);
+        //End Request
+        
+        //start
+        id++;
+        diff = System.currentTimeMillis() - receive;
+        crit = 4;
+        start = System.currentTimeMillis();
+        url = "http://192.168.2.10:8181/sensor/2/temp?crit="+crit;
+        HTTPRequest.sendGet(url);
+        receive = System.currentTimeMillis();
+        total = receive - start;
+        logToFile(fileName,id,crit,diff,start,receive,total);
+        //prime unit with data from sensor 2
+        //this data will remain for 10 sec in cache.
+        Thread.sleep(500);
+        //End Request
+        
+        //start
+        id++;
+        diff = System.currentTimeMillis() - receive;
+        crit = 1;
+        start = System.currentTimeMillis();
+        url = "http://192.168.2.10:8181/sensor/2/temp?crit="+crit;
+        HTTPRequest.sendGet(url);
+        receive = System.currentTimeMillis();
+        total = receive - start;
+        logToFile(fileName,id,crit,diff,start,receive,total);
+        //prime unit with data from sensor 2
+        //this data will remain for 10 sec in cache.
+        Thread.sleep(500);
+        //End Request
+        
+        //start
+        id++;
+        diff = System.currentTimeMillis() - receive;
+        crit = 4;
+        start = System.currentTimeMillis();
+        url = "http://192.168.2.10:8181/sensor/2/temp?crit="+crit;
+        HTTPRequest.sendGet(url);
+        receive = System.currentTimeMillis();
+        total = receive - start;
+        logToFile(fileName,id,crit,diff,start,receive,total);
+        //prime unit with data from sensor 2
+        //this data will remain for 10 sec in cache.
+        Thread.sleep(500);
+        //End Request
+        
+        //start
+        id++;
+        diff = System.currentTimeMillis() - receive;
+        crit = 4;
+        start = System.currentTimeMillis();
+        url = "http://192.168.2.10:8181/sensor/2/temp?crit="+crit;
+        HTTPRequest.sendGet(url);
+        receive = System.currentTimeMillis();
+        total = receive - start;
+        logToFile(fileName,id,crit,diff,start,receive,total);
+        //prime unit with data from sensor 2
+        //this data will remain for 10 sec in cache.
+        Thread.sleep(500);
+        //End Request
+        
+        //start
+        id++;
+        diff = System.currentTimeMillis() - receive;
+        crit = 5;
+        start = System.currentTimeMillis();
+        url = "http://192.168.2.10:8181/sensor/2/temp?crit="+crit;
+        HTTPRequest.sendGet(url);
+        receive = System.currentTimeMillis();
+        total = receive - start;
+        logToFile(fileName,id,crit,diff,start,receive,total);
+        //prime unit with data from sensor 2
+        //this data will remain for 10 sec in cache.
+        Thread.sleep(500);
+        //End Request
+        
+        //Start request
+        id++;
+        diff = System.currentTimeMillis() - receive;
+        crit = 5;
+        start = System.currentTimeMillis();
+        url = "http://192.168.2.10:8181/sensor/2/temp?crit="+crit;
+        HTTPRequest.sendGet(url);
+        receive = System.currentTimeMillis();
+        total = receive - start;
+        logToFile(fileName,id,crit,diff,start,receive,total);
+        //prime unit with data from sensor 2
+        //this data will remain for 10 sec in cache.
+        Thread.sleep(500);
+        //End Request
+        
+        //start
+        id++;
+        diff = System.currentTimeMillis() - receive;
+        crit = 5;
+        start = System.currentTimeMillis();
+        url = "http://192.168.2.10:8181/sensor/2/temp?crit="+crit;
+        HTTPRequest.sendGet(url);
+        receive = System.currentTimeMillis();
+        total = receive - start;
+        logToFile(fileName,id,crit,diff,start,receive,total);
+        //prime unit with data from sensor 2
+        //this data will remain for 10 sec in cache.
+        Thread.sleep(500);
+        //End Request
+        
+        //start
+        id++;
+        diff = System.currentTimeMillis() - receive;
+        crit = 5;
+        start = System.currentTimeMillis();
+        url = "http://192.168.2.10:8181/sensor/2/temp?crit="+crit;
+        HTTPRequest.sendGet(url);
+        receive = System.currentTimeMillis();
+        total = receive - start;
+        logToFile(fileName,id,crit,diff,start,receive,total);
+        //prime unit with data from sensor 2
+        //this data will remain for 10 sec in cache.
+        Thread.sleep(500);
+        //End Request
+        
+        //start
+        id++;
+        diff = System.currentTimeMillis() - receive;
+        crit = 4;
+        start = System.currentTimeMillis();
+        url = "http://192.168.2.10:8181/sensor/2/temp?crit="+crit;
+        HTTPRequest.sendGet(url);
+        receive = System.currentTimeMillis();
+        total = receive - start;
+        logToFile(fileName,id,crit,diff,start,receive,total);
+        //prime unit with data from sensor 2
+        //this data will remain for 10 sec in cache.
+        Thread.sleep(500);
+        //End Request
+        
+        //start
+        id++;
+        diff = System.currentTimeMillis() - receive;
+        crit = 4;
+        start = System.currentTimeMillis();
+        url = "http://192.168.2.10:8181/sensor/2/temp?crit="+crit;
+        HTTPRequest.sendGet(url);
+        receive = System.currentTimeMillis();
+        total = receive - start;
+        logToFile(fileName,id,crit,diff,start,receive,total);
+        //prime unit with data from sensor 2
+        //this data will remain for 10 sec in cache.
+        Thread.sleep(500);
+        //End Request
+        
+        //start
+        id++;
+        diff = System.currentTimeMillis() - receive;
+        crit = 1;
+        start = System.currentTimeMillis();
+        url = "http://192.168.2.10:8181/sensor/2/temp?crit="+crit;
+        HTTPRequest.sendGet(url);
+        receive = System.currentTimeMillis();
+        total = receive - start;
+        logToFile(fileName,id,crit,diff,start,receive,total);
+        //prime unit with data from sensor 2
+        //this data will remain for 10 sec in cache.
+        Thread.sleep(500);
+        //End Request
+        
+        //start
+        id++;
+        diff = System.currentTimeMillis() - receive;
+        crit = 5;
+        start = System.currentTimeMillis();
+        url = "http://192.168.2.10:8181/sensor/2/temp?crit="+crit;
+        HTTPRequest.sendGet(url);
+        receive = System.currentTimeMillis();
+        total = receive - start;
+        logToFile(fileName,id,crit,diff,start,receive,total);
+        //prime unit with data from sensor 2
+        //this data will remain for 10 sec in cache.
+        Thread.sleep(500);
+        //End Request
+        
+        //start
+        id++;
+        diff = System.currentTimeMillis() - receive;
+        crit = 4;
+        start = System.currentTimeMillis();
+        url = "http://192.168.2.10:8181/sensor/2/temp?crit="+crit;
+        HTTPRequest.sendGet(url);
+        receive = System.currentTimeMillis();
+        total = receive - start;
+        logToFile(fileName,id,crit,diff,start,receive,total);
+        //prime unit with data from sensor 2
+        //this data will remain for 10 sec in cache.
+        Thread.sleep(500);
+        //End Request
     }
 
+    public static void initFile(String name) {
+        try {
+            PrintWriter pw = new PrintWriter(new File(name));
+            pw.write("");
+            pw.close();
+            BufferedWriter bw = null;
+            bw = new BufferedWriter(new FileWriter(new File(name), true));
+            bw.write("Request ID;"
+                    + "Request Criticality (1-5);"
+                    + "Time since last request in ms;"
+                    + "Time Sent in ms since epoch;"
+                    + "Time Received anaswer in ms since epoch;"
+                    + "Total Time in ms");
+            bw.newLine();
+            bw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(LoadGen01.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void logToFile(String name,int id,int crit,long diff, long sent, long received, long total) {
+        try {
+            BufferedWriter bw = null;
+            bw = new BufferedWriter(new FileWriter(new File(name), true));
+            bw.write(id+";"
+                    + crit+";"
+                    + diff +";"
+                    + sent + ";"
+                    + received +";"
+                    + total);
+            bw.newLine();
+            bw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(LoadGen01.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
