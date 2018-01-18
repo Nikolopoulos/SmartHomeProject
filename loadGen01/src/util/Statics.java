0625dbf1d8f5ecaadc9297e0e6cdc1d84ce32de0
Basil Nikolopoulos <nikolopoulosbasil@gmail.com>
2018-01-18 14:07:44 +0200

    fixit

---
+++ loadGen01/src/util/Statics.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author Basil Nikolopoulos <nikolopoulosbasil.com>
+ */
+public class Statics {
+    public static String url = "http://192.168.0.100:8181";
+}
